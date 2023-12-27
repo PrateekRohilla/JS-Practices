e6bf285fe686344da5d399889d08fc2824602ea2
PrateekRohilla <prateek98rohilla@gmail.com>
2023-12-28 00:57:16 +0530

    final - currency exchange

--- Currency converter/app.js
+++ Currency converter/app.js
@@ -1,6 +1,13 @@
 const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
-
 const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+window.addEventListener("load", () => {
+    updateExchangeRate();
+})
 
 for(let select of dropdowns){
     for(currCode in countryList){
@@ -29,3 +36,26 @@ const updateFlag = (element) => {
     img.src = newSrc;
 };
 
+btn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    updateExchangeRate();
+});
+
+const updateExchangeRate = async () => {
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+
+    if(amtVal == "" || amtVal < 1){
+        amtVal = 1;
+        amount.value = "1";
+    }
+
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[toCurr.value.toLowerCase()];
+
+    let finalAmount = amtVal * rate;
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+};
