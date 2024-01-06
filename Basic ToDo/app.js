ddc3868005d84c902b5919d5a48efabd55640dbd
PrateekRohilla <prateek98rohilla@gmail.com>
2024-01-06 14:09:33 +0530

    delete task

--- Basic ToDo/app.js
+++ Basic ToDo/app.js
@@ -4,9 +4,22 @@ const inp = document.querySelector("input");
 
 
 btn.addEventListener("click", () => {
+
     let task = document.createElement("li");
     task.innerText = inp.value;
     ul.appendChild(task);
+
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "delete";
+    delBtn.classList.add("del");
+    task.appendChild(delBtn);
+
     inp.value = "";
 });
 
+ul.addEventListener("click", function(event){
+    if(event.target.nodeName == "BUTTON"){
+        let listItem = event.target.parentElement;
+        listItem.remove();
+    }
+});
