69236995ee9a5b0a14a9089255f9af39fc68719c
Prateek Rohilla <prateek98rohilla@gmail.com>
2024-01-14 20:32:33 +0530

    render values fetch from API

--- Weather app/script.js
+++ Weather app/script.js
@@ -3,14 +3,25 @@ const searchBtn = document.getElementById("searchBtn");
 const weatherImg = document.querySelector(".weather-img");
 const temperature = document.querySelector(".temperature");
 const description = document.querySelector(".description");
-const humidity = document.querySelector(".humidity");
-const windSpeed = document.querySelector(".wind-speed");
+const humidity = document.getElementById("humidity");
+const windSpeed = document.getElementById("wind-speed");
 
 async function checkWeather(city){
     const apiKey = config.weatherAPIKey;
     const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
     const weatherData = await fetch(`${URL}`).then(response => response.json());
+    
+    //converting temperature to degree from kelvin
+    temperature.innerHTML = `${Math.round(weatherData.main.temp - 273.15)}°C`;
+    
+    description.innerHTML = `${weatherData.weather[0].description}`;
+    
+    humidity.innerHTML = `${weatherData.main.humidity}%`;
+
+    windSpeed.innerHTML = `${weatherData.wind.speed}Km/H`;
+
+    
 
     console.log(weatherData);
 }
